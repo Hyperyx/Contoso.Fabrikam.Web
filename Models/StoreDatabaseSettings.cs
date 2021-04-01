ab9e717f3c16be20301e26aba3b807982ab21d95
Support Hyperyx <support@hyperyx.com>
2021-04-01 23:57:50 +0000

    Updated IStoreSettings

--- Models/StoreDatabaseSettings.cs
+++ Models/StoreDatabaseSettings.cs
@@ -3,6 +3,7 @@ namespace Contoso.Fabrikam.Web.Models
     public class StoreDatabaseSettings : IStoreDatabaseSettings
     {
         public string BooksCollectionName { get; set; }
+        public string MoviesCollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
@@ -10,6 +11,7 @@ namespace Contoso.Fabrikam.Web.Models
     public interface IStoreDatabaseSettings
     {
         string BooksCollectionName { get; set; }
+        string MoviesCollectionName { get; set; }
         string ConnectionString { get; set; }
         string DatabaseName { get; set; }
     }
